06e5e6940c0da384276bba5dd4945e87bd7a13cc
Wasili Adamow <wasili.adamow@gmail.com>
2019-01-30 15:19:02 +0100

    added WPF

---
+++ AppserviceWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace AppserviceWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("WPF -- button clicked");
+
+            // TODO: send back stuff to UWP
+        }
+    }
+}

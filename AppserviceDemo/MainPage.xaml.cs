07f5add736bfa3403d76f71378933392067eac12
Wasili Adamow <wasili.adamow@gmail.com>
2019-01-30 15:13:49 +0100

    init UWP app

---
+++ AppserviceDemo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AppserviceDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("UWP -- button clicked");
+
+            // TODO: trigger bridge stuff
+        }
+    }
+}
